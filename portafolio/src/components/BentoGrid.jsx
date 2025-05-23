b4079f728371fce53e1d89a9e751f7171e8775fa
Javier Uberos <156229400+ElBuenoDeJavier@users.noreply.github.com>
2025-05-23 15:38:04 +0200

    test

--- portafolio/src/components/BentoGrid.jsx
+++ portafolio/src/components/BentoGrid.jsx
@@ -1,16 +1,24 @@
 export default function BentoGridDisplay(){
     return (
         <div className="bento-grid text-white font-sans-serif">
-            <button href="#" className="relative mb-10 cursor-pointer font-bold py-2 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out">
+            <a
+                href="#"
+                className="relative mb-10 cursor-pointer font-medium py-2 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out inline-block"
+            >
                 <span
-                  className="absolute -inset-2 rounded-lg bg-gradient-to-r from-indigo-600 via-slate-600 to-violet-600 opacity-50 blur-2xl"
+                    className="absolute -inset-2 rounded-lg bg-gradient-to-r from-indigo-600 via-slate-600 to-violet-600 opacity-50 blur-2xl"
                 ></span>
-                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black rounded-md">
-                    Descarga mi CV
+                <span className="relative px-5 py-2.5 bg-black rounded-4xl">
+                   <span className="bg-gradient-to-l from-purple-500 via-orange-500 to-yellow-500 text-transparent bg-clip-text">
+                    Descarga mi CV</span>
                 </span>
-            </button>
-            <h1 className="font-bold text-6xl mb-4">Javier Uberos <span className="bg-gradient-to-tl from-slate-800 via-violet-500 to-zinc-400 bg-clip-text text-transparent">Web Developer</span></h1>
-            
+            </a>
+            <h1 className="font-bold text-6xl mb-4">
+                Javier Uberos{" "}
+                <span className="bg-gradient-to-tl from-slate-800 via-violet-500 to-zinc-400 bg-clip-text text-transparent">
+                    Web Developer
+                </span>
+            </h1>
         </div>
     );
 };
