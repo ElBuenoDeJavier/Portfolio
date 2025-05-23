73bb3de8716d8acac4b22c4afb147efc350663ee
Javier Uberos <156229400+ElBuenoDeJavier@users.noreply.github.com>
2025-05-23 15:25:06 +0200

    Mejora

--- portafolio/src/components/BentoGrid.jsx
+++ portafolio/src/components/BentoGrid.jsx
@@ -1,20 +1,16 @@
 export default function BentoGridDisplay(){
     return (
         <div className="bento-grid text-white font-sans-serif">
-            <button className="relative cur">
-                <div
+            <button href="#" className="relative mb-10 cursor-pointer font-bold py-2 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out">
+                <span
                   className="absolute -inset-2 rounded-lg bg-gradient-to-r from-indigo-600 via-slate-600 to-violet-600 opacity-50 blur-2xl"
-                ></div>
-                Descarga mi CV
+                ></span>
+                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black rounded-md">
+                    Descarga mi CV
+                </span>
             </button>
-
-            <h1 className="font-bold text-6xl mb-4">Tailwind Text <span className="bg-gradient-to-tl from-slate-800 via-violet-500 to-zinc-400 bg-clip-text text-transparent">Gradients</span></h1>
-            <div className="bento-item">Item 1</div>
-            <div className="bento-item">Item 2</div>
-            <div className="bento-item">Item 3</div>
-            <div className="bento-item">Item 4</div>
-            <div className="bento-item">Item 5</div>
-            <div className="bento-item">Item 6</div>
+            <h1 className="font-bold text-6xl mb-4">Javier Uberos <span className="bg-gradient-to-tl from-slate-800 via-violet-500 to-zinc-400 bg-clip-text text-transparent">Web Developer</span></h1>
+            
         </div>
     );
 };
